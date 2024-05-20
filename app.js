ba5130ebd0b4552fd69821b2b58fb864ccd58faf
andreiusha228 <andreybukovsky08@gmail.com>
2024-05-20 09:29:13 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require("path");
+const bodyParser = require('body-parser');
+const { usersRouter, gamesRouter, categoriesRouter } = require('./routes');
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(
+    bodyParser.json(),
+    express.static(path.join(__dirname, "public")), 
+    usersRouter,
+    gamesRouter,
+    categoriesRouter
+);
+
+app.listen(PORT, () => {
+    console.log(`Server is running at PORT http://localhost:${PORT}`);
+});
+
