a9248b413e8eecb1ebe80a7a5246288a1837470d
andreiusha228 <andreybukovsky08@gmail.com>
2024-05-30 11:22:54 +0200

    added cors

--- routes/games.js
+++ routes/games.js
@@ -1,12 +1,12 @@
 const { sendAllGames, sendGameCreated, sendGameUpdated, sendGameDeleted } = require('../controllers/games');
 const { checkAuth } = require('../middlewares/auth');
-const { findAllGames, createGame, updateGame, checkEmptyFields, checkIsGameExists, checkIfCategoriesAvaliable, checkIfUsersAreSafe, findGameById, deleteGame } = require('../middlewares/games');
+const { findAllGames, createGame, updateGame, checkEmptyFields, checkIsGameExists, checkIfCategoriesAvaliable, checkIfUsersAreSafe, findGameById, deleteGame, checkIsVoteRequest } = require('../middlewares/games');
 
 const gamesRouter = require('express').Router();
 
 gamesRouter.get("/games", findAllGames, sendAllGames);
 gamesRouter.post("/games", checkIsGameExists, checkIfCategoriesAvaliable ,  checkEmptyFields, checkAuth, createGame, sendGameCreated);
-gamesRouter.put("/games/:id", findGameById, checkIfUsersAreSafe, checkIfCategoriesAvaliable , checkEmptyFields, checkAuth, updateGame, sendGameUpdated);
+gamesRouter.put("/games/:id", findGameById, checkIsVoteRequest, checkIfUsersAreSafe, checkIfCategoriesAvaliable , checkEmptyFields, checkAuth, updateGame, sendGameUpdated);
 gamesRouter.delete("/games/:id", checkAuth, deleteGame, sendGameDeleted);
 
 module.exports = gamesRouter;
